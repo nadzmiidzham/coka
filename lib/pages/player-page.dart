8200bb67e470b900bf5c2b4b58834efca39c91e3
SeladangHijau <nadzmiidzham@gmail.com>
2020-04-05 16:17:48 +0800

    Changed player page background

--- lib/pages/player-page.dart
+++ lib/pages/player-page.dart
@@ -5,7 +5,6 @@ class PlayerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.black,
       child: Center(
         child: ListView(
           children: <Widget>[
