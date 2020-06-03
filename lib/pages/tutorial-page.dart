6fe7a5386037748b7520c5338bb5c2e07dc3ea3d
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2020-06-03 23:37:05 +0800

    finished adding tutorial pages

--- lib/pages/tutorial-page.dart
+++ lib/pages/tutorial-page.dart
@@ -1,6 +1,7 @@
+import 'package:coka/models/Tutorial.dart';
+import 'package:coka/providers/tutorial-provider.dart';
 import 'package:flutter/material.dart';
-
-// TODO: show list of tutorials
+import 'package:provider/provider.dart';
 
 class TutorialPage extends StatelessWidget {
   @override
@@ -9,7 +10,33 @@ class TutorialPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Tutorial'),
       ),
-      body: Container(),
+      body: Container(
+        child: ListView(
+          children: _tutorialListWidget(context),
+        ),
+      ),
     );
+  }
+
+  List<Widget> _tutorialListWidget(BuildContext context) {
+    List<Tutorial> tutorialList = Provider.of<TutorialProvider>(context, listen: false).tutorialList;
+    List<Widget> tempWidgetList = [];
+
+    for(int x=0 ; x<tutorialList.length ; x++) {
+      tempWidgetList.add(Consumer<TutorialProvider>(
+        builder: (context, provider, child) {
+          return CheckboxListTile(
+            title: Text(tutorialList[x].title),
+            value: tutorialList[x].isComplete,
+            onChanged: (bool value) {
+              tutorialList[x].isComplete = value;
+              provider.updateTutorial(tutorialList[x]);
+            },
+          );
+        },
+      ));
+    }
+
+    return tempWidgetList;
   }
 }
