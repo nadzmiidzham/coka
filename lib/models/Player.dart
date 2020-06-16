a3c55bee6c8ec6a60eacf804488706c1ad2de1d4
Muhammad Nadzmi <nadzmiidzham@gmail.com>
2020-06-16 23:51:59 +0800

    added player color attribute

--- lib/models/Player.dart
+++ lib/models/Player.dart
@@ -2,27 +2,36 @@ import 'package:coka/models/Ability.dart';
 import 'package:coka/models/StatusImpairment.dart';
 import 'package:coka/models/Worker.dart';
 
+enum PlayerColor {
+  GREEN,
+  BLUE,
+  RED,
+  YELLOW
+}
+
 class Player {
   String name;
   String imagePath;
   int level;
   int maxHp, curHp;
   String description;
+  PlayerColor color;
   List<Ability> abilityList;
-  List<Ability> workerAbilityList;
   List<StatusImpairment> statusImpairmentList;
   List<Worker> workerList;
+  List<Ability> workerAbilityList;
 
   Player({
     this.name,
+    this.imagePath,
     this.level,
     this.maxHp,
     this.curHp,
-    this.abilityList,
-    this.workerAbilityList,
-    this.imagePath,
     this.description,
+    this.color,
+    this.abilityList,
     this.statusImpairmentList,
     this.workerList,
+    this.workerAbilityList,
   });
 }
